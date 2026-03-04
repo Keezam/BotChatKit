d45d7a27cc698ba5155dbd4032fc0246ab97d4e9
Keezam <kevinzambrano53@gmail.com>
2026-03-04 17:22:51 -0500

    Update start.js

--- api/start.js
+++ api/start.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   // 🔓 CORS (PERMITIR TU DOMINIO)
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://www.grupasa.com"
+    "https://aigpsa.grupasa.com"
   );
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
